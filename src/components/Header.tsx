f177e2d12d69fd25f3be845326b89ae583e2b8a3
mrevanzak <mrevanzak@gmail.com>
2025-12-19 17:08:04 +0700

    feat: update header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,17 @@ import Logo from '~/svg/Logo.svg';
 
 export function Header() {
   return (
-    <header className='fixed z-50 flex top-4 w-full'>
-      <nav className='inline-flex bg-background-secondary border-border-secondary mx-auto items-center gap-3 px-6 py-3 rounded-full bg-card/80 border border-border backdrop-blur-sm'>
+    <header className='sticky z-50 mt-4 mb-6 flex w-full px-[16px]'>
+      <nav className='xs:flex-row bg-background-secondary border-border-secondary xs:w-fit xs:rounded-full bg-card/80 border-border xs:py-3 mx-auto inline-flex w-full flex-col items-center justify-center gap-3 rounded-4xl border px-6 py-4 backdrop-blur-sm'>
         <div className='flex items-center gap-1.5'>
           <Logo className='size-6' />
           <span className='font-medium'>SOLANAFUNDED</span>
         </div>
-        <div className='w-px h-3 bg-border-secondary'></div>
-        <span className='text-sm text-secondary-700 font-medium text-secondary'>
+
+        <div className='bg-border-secondary max-xs:hidden h-3 w-px'></div>
+        <div className='bg-border-secondary xs:hidden h-px w-full'></div>
+
+        <span className='text-secondary-700 text-secondary text-sm font-medium'>
           Memecoin Prop Trading
         </span>
       </nav>
